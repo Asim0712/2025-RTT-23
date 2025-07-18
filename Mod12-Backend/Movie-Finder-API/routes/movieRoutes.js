c17c099857dfe93678e967a243e3692f944d6e81
Asim0712 <asimdaud0@gmail.com>
2025-07-17 22:40:02 -0400

    Movie-Finder-API

---
+++ Mod12-Backend/Movie-Finder-API/routes/movieRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { searchMovies, getMovieDetails } from "../controllers/movieController.js";
+const router = Router();
+
+router.get("/search", searchMovies);
+router.get("/movies/:id", getMovieDetails);
+
+export default router;
--- Mod12-Backend/Movie-Finder-API/server.js
+++ Mod12-Backend/Movie-Finder-API/server.js
@@ -1,15 +1,24 @@
-import express from 'express';
-import axios from 'axios';
-
-
+import express from "express";
+import "dotenv/config";
+import morgan from "morgan";
+import movieRoutes from "./routes/movieRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+app.use(morgan("dev"));
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/api", movieRoutes);
 
-app.get('/movies',  (req, res) => {
-    res.send("welcome to movie finder api");
+app.use((req, res) => {
+  res.status(404).send("<h1>404 - Page Not Found</h1>");
 });
 
-app.listen(PORT, ()=>{"Server is running at port ", `${PORT}`});
+app.listen(PORT, () => {
+  console.log(`Server is running on" ${PORT}`);
+});
